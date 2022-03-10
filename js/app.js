249e009e1ddd3ce85c8c9aa2e21fdd40fa9b025a
rashelmahomud <rashelmahmudraj1998@gmail.com>
2022-03-11 02:44:14 +0600

    comment bugh fixed

--- js/app.js
+++ js/app.js
@@ -26,9 +26,15 @@ const reportPost = (id) => {
     showPosts(remainingPosts);
 };
 
+
 const displayContent = (text) => {
-    return text.length < 30 ? 'text' : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
+  if(text.length > 30){
+    return text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
+  } else{
+    return text;
+  }
 };
+
 
 const switchTab = (id) => {
     if (id === "posts") {
