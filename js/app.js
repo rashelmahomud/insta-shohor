841095ba1547827df571d89edd0338d744b14a65
rashelmahomud <rashelmahmudraj1998@gmail.com>
2022-03-11 11:56:47 +0600

    hundeling fack add

--- js/app.js
+++ js/app.js
@@ -22,6 +22,7 @@ const addToLiked = (id) => {
 
 const reportPost = (id) => {
     reportedPostsId.push(id);
+    // console.log(reportedPostsId);
     const remainingPosts = posts.filter((post) => !reportedPostsId.includes(post.id));
     showPosts(remainingPosts);
 };
@@ -158,6 +159,7 @@ const displayLikedPosts = () => {
 };
 
 const displayReportedPosts = () => {
+  document.getElementById( "reported" ).innerHTML = '';
     const reportedPosts = getReportedPosts();
     
     reportedPosts.forEach((post) => {
